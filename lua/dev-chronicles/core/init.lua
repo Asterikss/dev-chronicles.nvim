79496da2573bf221af8b3c4f650bd8dc57bae6de
Asterikss <AndreAndreM@proton.me>
2025-07-31 22:03:29 +0200

    rewrite _record_session to accept session_active

--- lua/dev-chronicles/core/init.lua
+++ lua/dev-chronicles/core/init.lua
@@ -203,21 +203,10 @@ M.end_session = function(data_file, track_days, min_session_time, extend_today_t
   M.abort_session()
 end
 
---- Never updates first_worked for an existing project, regardless of end_ts
----@param project_id string
----@param duration_sec integer
----@param end_ts integer
----@param track_days boolean
----@param extend_today_to_4am boolean
 ---@param data_file string
-M._record_session = function(
-  project_id,
-  duration_sec,
-  end_ts,
-  track_days,
-  extend_today_to_4am,
-  data_file
-)
+---@param session_active chronicles.SessionActive
+---@param track_days boolean
+M._record_session = function(data_file, session_active, track_days)
   local time = require('dev-chronicles.core.time')
   local data = require('dev-chronicles.utils.data').load_data(data_file)
   if not data then
@@ -227,37 +216,40 @@ M._record_session = function(
     return
   end
 
-  local normalized_end_ts = end_ts
-  local day_key = time.get_day_str(end_ts, extend_today_to_4am)
-  if day_key ~= time.get_day_str(end_ts) then
-    normalized_end_ts = time.convert_day_str_to_timestamp(day_key, true)
-  end
+  local canonical_end_ts = session_active.canonical_ts
+  local end_ts = session_active.now_ts
+  local today_key = session_active.canonical_today_str
+  local duration_sec = session_active.session_time_seconds
+  local project_id = session_active.project_id
 
   data.global_time = data.global_time + duration_sec
   data.last_data_write = end_ts
 
-  if not data.projects[project_id] then
-    data.projects[project_id] = {
+  local project = data.projects[project_id]
+  if not project then
+    ---@type chronicles.ChroniclesData.ProjectData
+    project = {
       total_time = 0,
-      first_worked = normalized_end_ts,
-      last_worked = normalized_end_ts,
-      last_worked_for_sort = end_ts,
+      first_worked = canonical_end_ts,
+      last_worked = end_ts,
+      last_worked_canonical = canonical_end_ts,
       by_month = {},
       by_day = {},
       tags_map = {},
     }
+    data.projects[project_id] = project
   end
 
-  local project = data.projects[project_id]
+  project.first_worked = math.min(project.first_worked, canonical_end_ts)
   project.total_time = project.total_time + duration_sec
-  project.last_worked = normalized_end_ts
-  project.last_worked_for_sort = end_ts
+  project.last_worked_canonical = canonical_end_ts
+  project.last_worked = end_ts
 
-  local curr_month = time.get_month_str(normalized_end_ts)
+  local curr_month = time.get_month_str(canonical_end_ts)
   project.by_month[curr_month] = (project.by_month[curr_month] or 0) + duration_sec
 
   if track_days then
-    project.by_day[day_key] = (project.by_day[day_key] or 0) + duration_sec
+    project.by_day[today_key] = (project.by_day[today_key] or 0) + duration_sec
   end
 
   require('dev-chronicles.utils.data').save_data(data, data_file)
