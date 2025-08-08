4d65897448971d27d7bb724c3e49f8208f0a6e5b
Asterikss <AndreAndreM@proton.me>
2025-08-08 21:11:23 +0200

    move end_session and record_session to session_ops

--- lua/dev-chronicles/core/session_ops.lua
+++ lua/dev-chronicles/core/session_ops.lua
@@ -39,4 +39,75 @@ M.update_chronicles_data_with_curr_session = function(data, session_active)
   return data
 end
 
+---@param data_file string
+---@param track_days boolean
+---@param min_session_time integer
+---@param extend_today_to_4am boolean
+M.end_session = function(data_file, track_days, min_session_time, extend_today_to_4am)
+  local _, session_active =
+    require('dev-chronicles.core.state').get_session_info(extend_today_to_4am)
+  if not session_active then
+    vim.notify('Dev Chronicles Error: Tried to end the session when session in not active')
+    return
+  end
+
+  if session_active.session_time_seconds >= min_session_time then
+    M._record_session(data_file, session_active, track_days)
+  end
+
+  require('dev-chronicles.core.state').abort_session()
+end
+
+---@param data_file string
+---@param session_active chronicles.SessionActive
+---@param track_days boolean
+M._record_session = function(data_file, session_active, track_days)
+  local time = require('dev-chronicles.core.time')
+  local data = require('dev-chronicles.utils.data').load_data(data_file)
+  if not data then
+    vim.notify(
+      'DevChronicles Error: Recording the session failed. No data returned from load_data()'
+    )
+    return
+  end
+
+  local canonical_end_ts = session_active.canonical_ts
+  local end_ts = session_active.now_ts
+  local today_key = session_active.canonical_today_str
+  local duration_sec = session_active.session_time_seconds
+  local project_id = session_active.project_id
+
+  data.global_time = data.global_time + duration_sec
+  data.last_data_write = end_ts
+
+  local project = data.projects[project_id]
+  if not project then
+    ---@type chronicles.ChroniclesData.ProjectData
+    project = {
+      total_time = 0,
+      first_worked = canonical_end_ts,
+      last_worked = end_ts,
+      last_worked_canonical = canonical_end_ts,
+      by_month = {},
+      by_day = {},
+      tags_map = {},
+    }
+    data.projects[project_id] = project
+  end
+
+  project.first_worked = math.min(project.first_worked, canonical_end_ts)
+  project.total_time = project.total_time + duration_sec
+  project.last_worked_canonical = canonical_end_ts
+  project.last_worked = end_ts
+
+  local curr_month = time.get_month_str(canonical_end_ts)
+  project.by_month[curr_month] = (project.by_month[curr_month] or 0) + duration_sec
+
+  if track_days then
+    project.by_day[today_key] = (project.by_day[today_key] or 0) + duration_sec
+  end
+
+  require('dev-chronicles.utils.data').save_data(data, data_file)
+end
+
 return M
