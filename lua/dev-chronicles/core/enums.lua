7f213d3ffe5153b453ec059e246eeec99161487c
Asterikss <AndreAndreM@proton.me>
2025-08-15 22:25:38 +0200

    add enums.lua

---
+++ lua/dev-chronicles/core/enums.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.PanelType = {
+  Dashboard = 'Dashboard',
+  Timeline = 'Timeline',
+  Stats = 'Stats',
+  All = 'All',
+}
+
+M.PanelSubtype = {
+  Days = 'Days',
+  Months = 'Months',
+  Years = 'Years',
+  All = 'All',
+}
+
+return M
