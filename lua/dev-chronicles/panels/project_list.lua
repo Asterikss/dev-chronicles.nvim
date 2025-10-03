46f829fa151455dcd4abfb2fde0eac473d0c2958
Asterikss <AndreAndreM@proton.me>
2025-10-03 21:31:45 +0200

    feat(project list panel): restructure _mark_project to take toggle_selection and callback as arguments. Update D mapping to mark projects for deletion. + small changes

--- lua/dev-chronicles/panels/project_list.lua
+++ lua/dev-chronicles/panels/project_list.lua
@@ -2,7 +2,11 @@ local M = {}
 
 local colors = require('dev-chronicles.core.colors')
 local render = require('dev-chronicles.core.render')
+local notify = require('dev-chronicles.utils.notify')
 
+---@class chronicles.ProjectList.Changes
+---@field new_colors? table<string, string>
+---@field to_be_deleted? table<string, boolean>
 M._changes = {}
 
 function M.display_project_list(opts)
@@ -22,14 +26,23 @@ function M.display_project_list(opts)
     ['?'] = function(_)
       M._show_project_help()
     end,
-    ['D'] = function(context)
-      M._mark_project(data.projects, context, 'D', 'DevChroniclesRed')
-    end,
     ['I'] = function(context)
       M._show_project_info(data.projects, context)
     end,
     ['C'] = function(context)
       M._change_project_color(data.projects, context)
+    end,
+    ['D'] = function(context)
+      M._mark_project(data.projects, context, 'D', 'DevChroniclesRed', true, function(project_id)
+        if not M._changes.to_be_deleted then
+          M._changes.to_be_deleted = {}
+        end
+        if M._changes.to_be_deleted[project_id] == nil then
+          M._changes.to_be_deleted[project_id] = true
+        else
+          M._changes.to_be_deleted[project_id] = nil
+        end
+      end)
     end,
   }
 
@@ -67,29 +80,20 @@ function M.display_project_list(opts)
 end
 
 function M._show_project_help()
-  local help_lines = {
+  local lines = {
     'Help | Project List',
     '',
     'Keybindings:',
     '  I     - Show project information',
+    "  C     - Change project's color",
     '  D     - Mark project for deletion',
     '  ?     - Show this help',
     '  q/Esc - Close window',
   }
-  local max_width = 0
-  for _, line in ipairs(help_lines) do
-    max_width = math.max(max_width, #line)
-  end
 
-  local title = help_lines[1]
-  local title_len = #title
-  if title_len < max_width then
-    local padding = math.floor((max_width - title_len) / 2)
-    help_lines[1] = string.rep(' ', padding) .. title
-  end
-
-  local highlights, len_help_lines = {}, #help_lines
-  for i = 1, len_help_lines do
+  local max_width, highlights, n_lines = 0, {}, #lines
+  for i = 1, n_lines do
+    max_width = math.max(max_width, #lines[i])
     highlights[i] = {
       line = i,
       col = 0,
@@ -98,15 +102,22 @@ function M._show_project_help()
     }
   end
 
+  local title = lines[1]
+  local title_len = #title
+  if title_len < max_width then
+    local padding = math.floor((max_width - title_len) / 2)
+    lines[1] = string.rep(' ', padding) .. title
+  end
+
   render.render({
-    lines = help_lines,
+    lines = lines,
     highlights = highlights,
     buf_name = 'Dev Chronicles Project List - Help',
     window_dimensions = {
       col = math.floor((vim.o.columns - max_width) / 2),
-      row = math.floor((vim.o.lines - len_help_lines) / 2),
+      row = math.floor((vim.o.lines - n_lines) / 2),
       width = max_width,
-      height = len_help_lines,
+      height = n_lines,
     },
   })
 end
@@ -161,11 +172,15 @@ end
 
 ---@param data_projects chronicles.ChroniclesData.ProjectData[]
 ---@param context chronicles.Panel.Context
-function M._mark_project(data_projects, context, symbol, hl_name)
+---@param symbol string: char
+---@param hl_name string
+---@param toggle_selection boolean
+---@param callback? function
+function M._mark_project(data_projects, context, symbol, hl_name, toggle_selection, callback)
   local project_name = context.line_content:sub(3)
 
   local marked_line
-  if context.line_content:sub(1, 1) == symbol then
+  if toggle_selection and context.line_content:sub(1, 1) == symbol then
     marked_line = '  ' .. project_name
   else
     marked_line = symbol .. ' ' .. project_name
@@ -192,8 +207,11 @@ function M._mark_project(data_projects, context, symbol, hl_name)
   else
     colors.apply_highlight(context.buf, 'DevChroniclesAccent', context.line_idx - 1, 2, -1)
   end
-
   colors.apply_highlight(context.buf, hl_name, context.line_idx - 1, 0, 1)
+
+  if callback then
+    callback(project_name)
+  end
 end
 
 ---@param data_projects chronicles.ChroniclesData.ProjectData[]
@@ -206,21 +224,23 @@ function M._change_project_color(data_projects, context)
     return
   end
 
-  local current_color = project_data.color
+  local current_color, new_color = project_data.color, nil
+  local current_color_line_default, current_color_line_index, current_color_line_default_text_end =
+    'Current:  ', 4, 9
   local new_color_line_default, new_color_line_index, new_color_line_default_text_end =
     'New:      ', 5, 4
-  local new_color
 
   local lines = {
     project_name .. ' — Change Color',
     '',
     '',
-    'Current:  ' .. (current_color and current_color .. ' ████████' or 'None'),
+    current_color_line_default
+      .. (current_color and '#' .. current_color .. '  ████████' or 'None'),
     new_color_line_default,
     '',
     '',
     'Input "nil" to remove the existing color',
-    'Press c/C to change the color again',
+    'Press C to change the color again',
     'Press Enter to confirm',
     'Press q/Esc to cancel',
   }
@@ -236,10 +256,22 @@ function M._change_project_color(data_projects, context)
     }
   end
 
+  if current_color then
+    highlights[n_lines + 1] = {
+      line = current_color_line_index,
+      col = current_color_line_default_text_end,
+      end_col = -1,
+      hl_group = colors._get_or_create_highlight(current_color),
+    }
+  end
+
   local function prompt(buf)
     vim.ui.input({
       prompt = 'Enter new hex color: ',
     }, function(user_input)
+      if not user_input then
+        return
+      end
       local hex_candidate = colors.check_and_normalize_hex_color(user_input)
       local new_color_line = new_color_line_default
 
@@ -289,7 +321,7 @@ function M._change_project_color(data_projects, context)
     M._changes.new_colors[project_name] = new_color
     vim.api.nvim_win_close(win, true)
     data_projects[project_name].color = new_color
-    M._mark_project(data_projects, context, 'C', 'DevChroniclesLightPurple')
+    M._mark_project(data_projects, context, 'C', 'DevChroniclesBlue', false)
   end
 
   local buf = render.render({
