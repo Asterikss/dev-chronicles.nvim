7e08b49e587020f7fae66ceab79dca2f848b36e2
Asterikss <AndreAndreM@proton.me>
2025-11-21 16:47:03 +0100

    refactor(dashboard): refactor set_hline_lines_hl to take optional len_lines argument and return new n_lines

--- lua/dev-chronicles/dashboard/content.lua
+++ lua/dev-chronicles/dashboard/content.lua
@@ -483,14 +483,18 @@ end
 ---@param win_width integer
 ---@param char? string
 ---@param hl_group? string
-function M.set_hline_lines_hl(lines, highlights, win_width, char, hl_group)
-  table.insert(lines, string.rep(char or '▔', win_width))
+---@param len_lines? integer
+---@return integer: len_lines
+function M.set_hline_lines_hl(lines, highlights, win_width, char, hl_group, len_lines)
+  len_lines = (len_lines or #lines) + 1
+  lines[len_lines] = string.rep(char or '▔', win_width)
   table.insert(highlights, {
-    line = #lines,
+    line = len_lines,
     col = 0,
     end_col = -1,
     hl_group = hl_group or DefaultColors.DevChroniclesGlobalProjectTime,
   })
+  return len_lines
 end
 
 ---@param lines string[]
