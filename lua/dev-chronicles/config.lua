a6c302e2684c289dad0ae53afe28fc8506edb33b
Asterikss <AndreAndreM@proton.me>
2025-11-28 22:03:31 +0100

    refactor(config): move dashboard section specific config to config/dashboard_cfg

--- lua/dev-chronicles/config.lua
+++ lua/dev-chronicles/config.lua
@@ -1,89 +1,11 @@
 local M = {}
 
 local notify = require('dev-chronicles.utils.notify')
+local dashboard_cfg = require('dev-chronicles.config.dashboard_cfg')
+local default_dashboard_vars = dashboard_cfg.default_dashboard_vars
 
 ---@type chronicles.Options
 local options
-
----@type chronicles.Options.Dashboard.DefaultVars
-local default_dashboard_vars = {
-  bar_width = 9,
-  bar_header_extends_by = 1,
-  bar_footer_extends_by = 1,
-  bar_spacing = 3,
-}
-
----@type chronicles.Options.Dashboard.Header
-local dashboard_section_header_opts = {
-  period_indicator = {
-    date_range = true,
-    days_count = true,
-    time_period_str = nil,
-    time_period_str_singular = nil,
-  },
-  show_current_session_time = true,
-  prettify = true,
-  window_title = ' Dev Chronicles ',
-  total_time = {
-    as_hours_max = true,
-    as_hours_min = true,
-    round_hours_ge_x = 1,
-    format_str = 'total time: %s',
-  },
-  project_global_time = {
-    enable = true,
-    show_only_if_differs = true,
-    color_like_bars = false,
-    as_hours_max = true,
-    as_hours_min = true,
-    round_hours_ge_x = 1,
-  },
-  top_projects = {
-    enable = true,
-    extra_space_between_bars = false,
-    wide_bars = false,
-    super_extra_duper_wide_bars = false,
-    min_top_projects_len_to_show = 1,
-  },
-}
-
----@type chronicles.Options.Dashboard.Sorting
-local dashboard_section_sorting_opts = {
-  enable = true,
-  sort_by_last_worked_not_total_time = true,
-  ascending = true,
-}
-
----@type chronicles.Options.Dashboard.Section
-local dashboard_section_base = {
-  header = dashboard_section_header_opts,
-  sorting = dashboard_section_sorting_opts,
-  dynamic_bar_height_thresholds = nil,
-  n_by_default = 2,
-  random_bars_coloring = false,
-  bars_coloring_follows_sorting_in_order = true,
-  min_proj_time_to_display_proj = 0,
-  window_height = 0.8,
-  window_width = 0.8,
-  window_border = nil,
-  bar_chars = nil,
-  project_total_time = {
-    as_hours_max = true,
-    as_hours_min = true,
-    round_hours_ge_x = 1,
-    color_like_bars = false,
-  },
-}
-
-local function make_dashboard_section(opts)
-  return vim.tbl_deep_extend(
-    'force',
-    dashboard_section_base,
-    { header = dashboard_section_header_opts },
-    { sorting = dashboard_section_sorting_opts },
-    opts or {}
-  )
-end
 
 ---@type chronicles.Options
 local defaults = {
@@ -114,7 +36,7 @@ local defaults = {
     footer = {
       let_proj_names_extend_bars_by_one = true,
     },
-    dashboard_days = make_dashboard_section({
+    dashboard_days = dashboard_cfg.make_dashboard_section({
       header = {
         window_title = ' Dev Chronicles Days ',
         period_indicator = {
@@ -125,7 +47,7 @@ local defaults = {
       n_by_default = 30,
       dynamic_bar_height_thresholds = { 2, 3.5, 5 }, -- It could be a integer[] and integer[][]
     }),
-    dashboard_months = make_dashboard_section({
+    dashboard_months = dashboard_cfg.make_dashboard_section({
       header = {
         window_title = ' Dev Chronicles Months ',
         top_projects = { wide_bars = true },
@@ -134,7 +56,7 @@ local defaults = {
       window_border = { '╬', '═', '╬', '║', '╬', '═', '╬', '║' },
       dynamic_bar_height_thresholds = nil, -- = { 15, 25, 40 },
     }),
-    dashboard_years = make_dashboard_section({
+    dashboard_years = dashboard_cfg.make_dashboard_section({
       header = {
         window_title = ' Dev Chronicles Years ',
         top_projects = { super_extra_duper_wide_bars = true },
@@ -143,7 +65,7 @@ local defaults = {
       sorting = { sort_by_last_worked_not_total_time = false },
       window_border = { '╬', '═', '╬', '║', '╬', '═', '╬', '║' },
     }),
-    dashboard_all = make_dashboard_section({
+    dashboard_all = dashboard_cfg.make_dashboard_section({
       header = {
         window_title = ' Dev Chronicles All ',
         total_time_format_str = 'global total time: %s',
---
+++ lua/dev-chronicles/config/dashboard_cfg.lua
@@ -0,0 +1,71 @@
+local M = {}
+
+---@type chronicles.Options.Dashboard.DefaultVars
+M.default_dashboard_vars = {
+  bar_width = 9,
+  bar_header_extends_by = 1,
+  bar_footer_extends_by = 1,
+  bar_spacing = 3,
+}
+
+function M.make_dashboard_section(opts)
+  ---@type chronicles.Options.Dashboard.Section
+  local dashboard_section_opts = {
+    header = {
+      period_indicator = {
+        date_range = true,
+        days_count = true,
+        time_period_str = nil,
+        time_period_str_singular = nil,
+      },
+      show_current_session_time = true,
+      prettify = true,
+      window_title = ' Dev Chronicles ',
+      total_time = {
+        as_hours_max = true,
+        as_hours_min = true,
+        round_hours_ge_x = 1,
+        format_str = 'total time: %s',
+      },
+      project_global_time = {
+        enable = true,
+        show_only_if_differs = true,
+        color_like_bars = false,
+        as_hours_max = true,
+        as_hours_min = true,
+        round_hours_ge_x = 1,
+      },
+      top_projects = {
+        enable = true,
+        extra_space_between_bars = false,
+        wide_bars = false,
+        super_extra_duper_wide_bars = false,
+        min_top_projects_len_to_show = 1,
+      },
+    },
+    sorting = {
+      enable = true,
+      sort_by_last_worked_not_total_time = true,
+      ascending = true,
+    },
+    dynamic_bar_height_thresholds = nil,
+    n_by_default = 2,
+    random_bars_coloring = false,
+    bars_coloring_follows_sorting_in_order = true,
+    min_proj_time_to_display_proj = 0,
+    window_height = 0.8,
+    window_width = 0.8,
+    window_border = nil,
+    bar_chars = nil,
+    project_total_time = {
+      as_hours_max = true,
+      as_hours_min = true,
+      round_hours_ge_x = 1,
+      color_like_bars = false,
+    },
+  }
+
+  return vim.tbl_deep_extend('force', dashboard_section_opts, opts or {})
+end
+
+return M
