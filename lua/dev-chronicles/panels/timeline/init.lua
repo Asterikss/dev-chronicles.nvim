28e4dfee7663c06c0fef6d9e2c8a49ad7263b206
Asterikss <AndreAndreM@proton.me>
2025-12-06 22:42:35 +0100

    feat(timeline): add create timeline content implementation

---
+++ lua/dev-chronicles/panels/timeline/init.lua
@@ -0,0 +1,155 @@
+local M = {}
+
+---@param timeline_data chronicles.Timeline.Data
+---@param timeline_type_opts chronicles.Options.Timeline.Section
+---@param win_width integer
+---@param win_height integer
+---@param plugin_opts chronicles.Options
+---@param curr_session_time? integer
+---@return string[], chronicles.Highlight[]
+function M._create_timeline_content(
+  timeline_data,
+  timeline_type_opts,
+  win_width,
+  win_height,
+  plugin_opts,
+  curr_session_time
+)
+  local timeline_logic = require('dev-chronicles.panels.timeline.logic')
+  local timeline_content = require('dev-chronicles.panels.timeline.content')
+  local utils = require('dev-chronicles.utils')
+  local timeline_opts = plugin_opts.timeline
+
+  local lines = {}
+  local highlights = {}
+
+  if timeline_data.segments == nil then
+    return timeline_content.handle_no_segments_lines_hl(
+      lines,
+      highlights,
+      timeline_data,
+      win_width,
+      win_height,
+      timeline_type_opts.header
+    )
+  end
+
+  local header_height = 4
+  local footer_height = (timeline_type_opts.segment_numeric_labels.enable and 1 or 0)
+    + (timeline_type_opts.segment_abbr_labels.enable and 1 or 0)
+  local chart_height = win_height - (header_height + footer_height)
+  local horizontal_margin = 2
+  local max_chart_width = win_width - (2 * horizontal_margin)
+  local vertical_space_for_bars = chart_height - 3 -- time labels row + gap 1 + chart floor
+  local n_segments = #timeline_data.segments
+
+  local n_segments_to_keep, chart_left_margin_col =
+    require('dev-chronicles.dashboard.logic').calc_chart_stats(
+      timeline_type_opts.bar_width,
+      timeline_type_opts.bar_spacing,
+      max_chart_width,
+      n_segments,
+      win_width
+    )
+
+  if n_segments_to_keep < 1 then
+    return timeline_content.handle_no_segments_lines_hl(
+      lines,
+      highlights,
+      timeline_data,
+      win_width,
+      win_height,
+      timeline_type_opts.header
+    )
+  end
+
+  if n_segments ~= n_segments_to_keep then
+    timeline_logic.cut_off_segments(timeline_data, n_segments, n_segments_to_keep)
+  end
+
+  local row_repr = utils.get_random_from_tbl(timeline_type_opts.row_repr or timeline_opts.row_repr)
+  local row_representation =
+    timeline_logic.construct_row_representation(row_repr, timeline_type_opts.bar_width)
+
+  local bar_distribution_data = timeline_logic.construct_bar_distribution_data(
+    timeline_data,
+    vertical_space_for_bars,
+    row_representation.row_codepoint_count
+  )
+
+  local len_lines = 0
+  len_lines = timeline_content.set_header_lines_hl(
+    lines,
+    highlights,
+    timeline_data,
+    timeline_type_opts.header,
+    win_width,
+    curr_session_time,
+    len_lines
+  )
+
+  len_lines = timeline_content.set_time_labels_above_bars_lines_hl(
+    lines,
+    highlights,
+    timeline_data,
+    timeline_type_opts.bar_width,
+    timeline_type_opts.bar_spacing,
+    win_width,
+    chart_left_margin_col,
+    timeline_type_opts.segment_time_labels,
+    len_lines
+  )
+
+  len_lines = timeline_content.set_bars_lines_hl(
+    lines,
+    highlights,
+    timeline_data,
+    row_representation,
+    bar_distribution_data,
+    vertical_space_for_bars,
+    chart_left_margin_col,
+    timeline_type_opts.bar_spacing,
+    win_width
+  )
+
+  len_lines = require('dev-chronicles.dashboard.content').set_hline_lines_hl(
+    lines,
+    highlights,
+    win_width,
+    nil,
+    nil,
+    len_lines
+  )
+
+  if timeline_type_opts.segment_numeric_labels.enable then
+    len_lines = timeline_content.set_numeric_labels_lines_hl(
+      lines,
+      highlights,
+      timeline_data,
+      timeline_type_opts.bar_width,
+      timeline_type_opts.bar_spacing,
+      win_width,
+      chart_left_margin_col,
+      timeline_type_opts.segment_numeric_labels,
+      len_lines
+    )
+  end
+
+  if timeline_type_opts.segment_abbr_labels.enable then
+    timeline_content.set_abbr_labels_lines_hl(
+      lines,
+      highlights,
+      timeline_data,
+      timeline_type_opts.bar_width,
+      timeline_type_opts.bar_spacing,
+      win_width,
+      chart_left_margin_col,
+      timeline_type_opts.segment_abbr_labels,
+      len_lines
+    )
+  end
+
+  return lines, highlights
+end
+
+return M
