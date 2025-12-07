7f563402549f77fd4eda10cf7c69b921c968738f
Asterikss <AndreAndreM@proton.me>
2025-12-07 12:20:51 +0100

    feat(timeline): add the main timeline function impl

--- lua/dev-chronicles/panels/timeline/init.lua
+++ lua/dev-chronicles/panels/timeline/init.lua
@@ -1,5 +1,114 @@
 local M = {}
 
+local utils = require('dev-chronicles.utils')
+
+---@param data chronicles.ChroniclesData
+---@param panel_subtype chronicles.Panel.Subtype
+---@param panel_subtype_args chronicles.Panel.Subtype.Args
+---@param opts chronicles.Options
+---@param session_base chronicles.SessionBase
+---@param session_time? integer
+---@return chronicles.Panel.Data?
+function M.timeline(data, panel_subtype, panel_subtype_args, opts, session_base, session_time)
+  local notify = require('dev-chronicles.utils.notify')
+  local timeline_data_extraction = require('dev-chronicles.panels.timeline.data_extraction')
+  local PanelSubtype = require('dev-chronicles.core.enums').PanelSubtype
+
+  ---@type chronicles.Timeline.Data?
+  local timeline_data
+  ---@type chronicles.Options.Timeline.Section?
+  local timeline_type_options
+
+  local start_offset = panel_subtype_args.start_offset
+  local end_offset = panel_subtype_args.end_offset
+  if
+    (start_offset and (start_offset < 0 or start_offset % 1 ~= 0))
+    or (end_offset and (end_offset < 0 or end_offset % 1 ~= 0))
+  then
+    notify.warn('Both start_offset and end_offset must be non-negative integers.')
+    return
+  end
+
+  if panel_subtype == PanelSubtype.Days then
+    if not opts.track_days.enable then
+      notify.warn('track_days option is disabled. Enable it to display day data.')
+      return
+    end
+
+    timeline_type_options = opts.timeline.timeline_days
+    timeline_data = timeline_data_extraction.get_timeline_data_days(
+      data,
+      session_base.canonical_today_str,
+      timeline_type_options.n_by_default,
+      timeline_type_options.header.period_indicator,
+      timeline_type_options.segment_abbr_labels,
+      opts.track_days.optimize_storage_for_x_days,
+      start_offset,
+      end_offset
+    )
+  elseif panel_subtype == PanelSubtype.Months then
+    timeline_type_options = opts.timeline.timeline_months
+    timeline_data = timeline_data_extraction.get_timeline_data_months(
+      data,
+      session_base,
+      timeline_type_options.n_by_default,
+      timeline_type_options.header.period_indicator,
+      timeline_type_options.segment_abbr_labels,
+      panel_subtype_args.start_date,
+      panel_subtype_args.end_date
+    )
+  elseif panel_subtype == PanelSubtype.Years then
+    timeline_type_options = opts.timeline.timeline_years
+    timeline_data = timeline_data_extraction.get_timeline_data_years(
+      data,
+      session_base,
+      timeline_type_options.n_by_default,
+      timeline_type_options.header.period_indicator,
+      panel_subtype_args.start_date,
+      panel_subtype_args.end_date
+    )
+  elseif panel_subtype == PanelSubtype.All then
+    timeline_type_options = opts.timeline.timeline_all
+    timeline_data = timeline_data_extraction.get_timeline_data_all(data, session_base)
+  else
+    notify.warn(
+      string.format(
+        "Unrecognized panel subtype for Timeline: '%s'.\nExpected one of: 'Days', 'Months', 'Years', or 'All'.",
+        tostring(panel_subtype)
+      )
+    )
+    return
+  end
+
+  if not timeline_data then
+    return
+  end
+
+  local window_dimensions = utils.get_window_dimensions(
+    timeline_type_options.window_width,
+    timeline_type_options.window_height
+  )
+
+  local lines, highlights = M._create_timeline_content(
+    timeline_data,
+    timeline_type_options,
+    window_dimensions.width,
+    window_dimensions.height,
+    opts,
+    session_time
+  )
+
+  ---@type chronicles.Panel.Data
+  return {
+    lines = lines,
+    highlights = highlights,
+    window_dimensions = window_dimensions,
+    buf_name = 'Dev Chronicles Timeline',
+    window_title = timeline_type_options.header.window_title,
+    window_boarder = nil,
+  }
+end
+
 ---@param timeline_data chronicles.Timeline.Data
 ---@param timeline_type_opts chronicles.Options.Timeline.Section
 ---@param win_width integer
@@ -17,7 +126,6 @@ function M._create_timeline_content(
 )
   local timeline_logic = require('dev-chronicles.panels.timeline.logic')
   local timeline_content = require('dev-chronicles.panels.timeline.content')
-  local utils = require('dev-chronicles.utils')
   local timeline_opts = plugin_opts.timeline
 
   local lines = {}
